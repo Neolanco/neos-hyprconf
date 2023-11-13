3d5cfb8fcae767c583f3a1a16e0307fb789eb4b4
Neolanco <www.leon098@gmail.com>
2023-11-13 14:12:21 +0100

    init mate

---
+++ background-extraction/write-general.style.css.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Read the contents of the colors file into 3 variables
+IFS=',' read -r color1 color2 color3 < ~/.config/hypr/background-extraction/extracted-colors.txt
+
+# Create a temporary file to store the new contents of the Carla file
+temp_file=$(mktemp)
+
+# Write the new contents to the temporary file
+echo "@define-color color-accent-0-0 #$color1;" > $temp_file
+echo "@define-color color-accent-0-1 #$color2;" >> $temp_file
+echo "@define-color color-accent-0-2 #$color3;" >> $temp_file
+
+# Append the rest of the hyprland file to the temporary file
+cat ~/.config/hypr/general.style.css | tail -n +4 >> $temp_file
+
+# Move the temporary file to the config file
+mv $temp_file ~/.config/hypr/general.style.css
--- you-colors.sh
+++ you-colors.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-~/.config/hypr/background-extraction/extract-colors.sh
+~/.config/hypr/background-extraction/extract-colors.sh #extracting module
 
-~/.config/hypr/background-extraction/write-hyprland.conf.sh
-~/.config/hypr/background-extraction/write-waybay-style.css.sh
-~/.config/hypr/background-extraction/write-anyrun-style.css.sh
+~/.config/hypr/background-extraction/write-hyprland.conf.sh # hyprland module
+~/.config/hypr/background-extraction/write-general.style.css.sh #general css module
+#~/.config/hypr/background-extraction/write-waybay-style.css.sh #waybar
+#~/.config/hypr/background-extraction/write-anyrun-style.css.sh #anyrun
